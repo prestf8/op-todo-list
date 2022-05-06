4c01c892fe0005238b8df9948e2f695131beac64
prestf8 <prestf8@gmail.com>
2022-05-06 00:41:06 -0700

    style and css loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import Task from "./modules/task.js";
 import { addTaskToDom } from "./modules/dom-modules.js";
+import "./stylesheets/styles.css";
+
 const addProjectBtn = document.getElementById("add-project-btn");
 
 addProjectBtn.addEventListener("click", function () {
