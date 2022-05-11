42e5741344a9755399eab7a7af772d6c68da26ed
prestf8 <prestf8@gmail.com>
2022-05-11 08:23:25 -0700

    style up create task interface

--- src/index.js
+++ src/index.js
@@ -5,14 +5,19 @@ const createTaskBtn = document.getElementById("create-task-btn");
 const titleInput = document.getElementById("title-input");
 const dueDateInput = document.getElementById("due-date-input");
 
+// not used yet
 const priorityInputs = document.querySelectorAll(
   "create-task-interface-priority-div > input[type=text]"
+);
+
+const selectedPriorityInput = document.querySelector(
+  "input[name='priority']:checked"
 );
 
 createTaskBtn.addEventListener("click", function () {
   domModules.addTaskToDom(
     titleInput.value,
     dueDateInput.value,
-    priorityInputs.value
+    selectedPriorityInput.value
   );
 });
