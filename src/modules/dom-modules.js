33980d4e94ab203779209660a04add01a5431f86
prestf8 <prestf8@gmail.com>
2022-05-13 07:57:49 -0700

    structure task dom better

--- src/modules/dom-modules.js
+++ src/modules/dom-modules.js
@@ -8,18 +8,33 @@ const domModules = (function () {
     return new Task(title, dueDate, priority);
   }
 
-  function addTaskToDom(title, dueDate, priority) {
+  function addTask(title, dueDate, priority) {
     const task = _createTask(title, dueDate, priority);
-
     MainStorage.addTaskToStorage(task); // Adds task to main storage
+    addTaskToDom(title, dueDate, priority);
+  }
 
-    const taskEle = document.createElement("div");
+  function addTaskToDom(title, dueDate, priority) {
+    const taskEle = document.createElement("div"); // div element that is going to be appended to DOM
+
+    // title
+    const taskTitleLabel = document.createElement("p");
     const titleTextNode = document.createTextNode(title);
+    taskTitleLabel.appendChild(titleTextNode);
+    taskEle.appendChild(taskTitleLabel); // main div appending title
+
+    // due Date
+    const taskDueDateLabel = document.createElement("p");
     const dueDateTextNode = document.createTextNode(dueDate);
+    taskDueDateLabel.appendChild(dueDateTextNode);
+    taskEle.appendChild(taskDueDateLabel);
+
+    // priority
+    const taskPriorityLabel = document.createElement("p");
     const priorityTextNode = document.createTextNode(priority);
-    taskEle.appendChild(titleTextNode);
-    taskEle.appendChild(dueDateTextNode);
-    taskEle.appendChild(priorityTextNode);
+    taskPriorityLabel.appendChild(priorityTextNode);
+    taskEle.appendChild(taskPriorityLabel);
+
     document.body.appendChild(taskEle);
   }
 
